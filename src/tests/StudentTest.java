05c626c6fc0dfd45bb29f718992459a692404a37
Yasaman <ys.jafari@ut.ac.ir>
2017-08-12 22:13:43 +0430

    Student-getReportCard tested.

--- src/tests/StudentTest.java
+++ src/tests/StudentTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import entities.Course;
+import entities.FinishedCourse;
+import entities.Grades;
 import entities.Student;
 
 public class StudentTest {
@@ -19,6 +21,23 @@ public class StudentTest {
 		harry = new Student(courses);
 		Vector<Course> actual = harry.getCourses();
 		Vector<Course> expected = courses;
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getReportCardTest() {
+		Vector <Course> courses = new Vector<Course>();
+		Vector <FinishedCourse> finishedCourses = new Vector<FinishedCourse>();
+	    Course potions = new Course("potions", "Severus Snape", 'C', 1995);
+	    //	public FinishedCourse(Grades grade, boolean passed, String name, String professorName, char minGrade, int year, Vector<String> studentNames){
+	    FinishedCourse flying = new FinishedCourse(Grades.O, true, "flying", "Madame Hooch", 'A', 1996, null);
+	    finishedCourses.add(flying);
+	    courses.add(potions);
+	    
+		harry = new Student(courses, finishedCourses, "harry");
+		Vector<FinishedCourse> actual = harry.getReportCard();
+		Vector<FinishedCourse> expected = finishedCourses;
 		
 		Assert.assertEquals(expected, actual);
 	}
