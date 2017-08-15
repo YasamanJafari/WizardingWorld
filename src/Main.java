587ad8a876939020fd7c06b97389690c69cb6fe8
Yasaman <ys.jafari@ut.ac.ir>
2017-08-15 18:46:04 +0430

    CourseService-setCourse tested.

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		//Creating an instance of courseService test in order to use the methods
 		CourseServiceTest cstest = new CourseServiceTest();
+		
+		//CourseService::getCourseTest
 		System.out.println("** COURSE_SERVICE::getCourse TEST **");
 		//Printing test result
 		System.out.println(cstest.getCourseTest());
 		System.out.println("----------------------------------------");
-	}
 
+		System.out.println("** COURSE_SERVICE::setCourse TEST **");
+		//Printing test result
+		System.out.println(cstest.setCourseTest());
+		System.out.println("----------------------------------------");
+	}
 }
--- src/serviceTest/CourseServiceTest.java
+++ src/serviceTest/CourseServiceTest.java
@@ -17,4 +17,18 @@ public class CourseServiceTest {
 			return true;
 		return false;
 	}
+	
+	public boolean setCourseTest(){
+		Professor snape = new Professor("Extremely skilled at potions and Occlumency.");
+		Course potions = new Course("potions", snape, Grades.A, 1995);
+		Course actual = potions;
+		
+		CourseService potionsService = new CourseService();
+		potionsService.setCourse(potions);
+		Course expected = potionsService.getCourse();
+		
+		if(actual.equals(expected))
+			return true;
+		return false;
+	}
 }
